7a46ad23474f96adc2b724a6ea3332aa2e59c221
Ovod2013 <Ovod2013@yandex.ru>
2023-05-22 13:24:49 +0300

    Сделал задачи 25, 27 и 29, работают

---
+++ task29/Program.cs
@@ -0,0 +1,17 @@
+﻿/*Задача 29: Напишите программу, которая задаёт
+ массив из 8 элементов и выводит их на экран.
+1, 2, 5, 7, 19, 4, 76, 3 -> [1, 2, 5, 7, 19,4,76,3]
+6, 1, 33, 43, 12, 3 ,2, 9 -> [6, 1, 33,43,12,3,2,9]*/
+
+
+int[] massiv = new int[8];
+Random rnd = new Random();
+//Console.WriteLine(rnd.Next(0, 99));
+
+for (int i=0; i<8; i++)
+{
+    massiv[i] = rnd.Next(0, 100);
+    if (i<7) {Console.Write(massiv[i] + ", ");}
+    else Console.Write(massiv[i]);
+}
+
